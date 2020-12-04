00cd367cd5181ead528ac2accf48a0ba31e70d8d
abDevII <barbey.arthur@gmail.com>
2020-12-04 11:13:04 +0100

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -21,7 +21,7 @@ function RenderDish({dish}) {
     );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, dishId}) {
     if (comments != null) {
         return (
             <div className="col-12 col-md-5 m-1">
@@ -34,7 +34,7 @@ function RenderComments({comments}) {
                         </ul>
                     );                            
                 })}
-                <CommentForm />                       
+                <CommentForm dishId={dishId} addComment={addComment} />                       
             </div>
         );
     }
@@ -61,7 +61,9 @@ const DishDetail = (props) => {
                 </div>             
                 <div className="row">
                     <RenderDish dish = {props.dish} />
-                    <RenderComments comments = {props.comments} />
+                    <RenderComments comments = {props.comments}
+                        addComment={props.addComment}
+                        dishId={props.dish.id} />
                 </div>
             </div>
         );            
@@ -93,8 +95,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Current State is: " + JSON.stringify(values));
-        alert("Current State is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
